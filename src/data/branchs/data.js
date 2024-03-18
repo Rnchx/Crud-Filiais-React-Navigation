c003eba9d8389ffa53a48593e0a53c9c6aa4af2a
Rnchx <jpcrocha8@gmail.com>
2024-03-18 10:37:03 -0300

    add folders data, model and add informations in folders like that datas

---
+++ src/data/branchs/data.js
@@ -0,0 +1,52 @@
+export const mockedBranchs = [
+  {
+    "nome": "Escola Estadual Castro Alves",
+    "fundacao": "1965-08-25",
+    "corPrimaria": "#ff0000", // Vermelho
+    "corSecundaria": "#ffffff", // Branco
+    "qntdFuncionarios": "50",
+    "qntddAlunos": "800",
+    "qntddTurmas": "30",
+    "bairro": "Centro",
+    "cidade": "São Paulo",
+    "cep": "01000-000",
+    "telefone": "(11) 1234-5678",
+    "email": "contato@escolacastroalves.com",
+    "nomeDoResponsavel": "Ana Santos",
+    "cargoDoResponsavel": "Diretora"
+  },
+
+  {
+    "nome": "Colégio Santa Tereza",
+    "fundacao": "1978-03-10",
+    "corPrimaria": "#008000", // Verde
+    "corSecundaria": "#ffffff", // Branco
+    "qntdFuncionarios": "40",
+    "qntddAlunos": "600",
+    "qntddTurmas": "25",
+    "bairro": "Botafogo",
+    "cidade": "Rio de Janeiro",
+    "cep": "22250-040",
+    "telefone": "(21) 9876-5432",
+    "email": "contato@santatereza.com",
+    "nomeDoResponsavel": "Carlos Silva",
+    "cargoDoResponsavel": "Diretor"
+  },
+
+  {
+    "nome": "Escola Municipal João XXIII",
+    "fundacao": "1990-11-15",
+    "corPrimaria": "#0000ff", // Azul
+    "corSecundaria": "#ffffff", // Branco
+    "qntdFuncionarios": "30",
+    "qntddAlunos": "400",
+    "qntddTurmas": "20",
+    "bairro": "Boa Viagem",
+    "cidade": "Recife",
+    "cep": "51021-310",
+    "telefone": "(81) 2345-6789",
+    "email": "contato@joaoxxiii.com",
+    "nomeDoResponsavel": "Marta Oliveira",
+    "cargoDoResponsavel": "Diretora"
+  }
+];
---
+++ src/models/Branch.js
@@ -0,0 +1,25 @@
+class Branch {
+  constructor(nome, fundacao, corPrimaria, corSecundaria, qntdFuncionarios, qntddAlunos, qntddTurmas, bairro, cidade, cep, telefone, email, nomeDoResponsavel, cargoDoResponsavel) {
+
+    this.nome = nome,
+      this.id = this.generateId(),
+      this.fundacao = fundacao,
+      this.corPrimaria = corPrimaria,
+      this.corSecundaria = corSecundaria,
+      this.qntdFuncionarios = qntdFuncionarios,
+      this.qntddAlunos = qntddAlunos,
+      this.qntddTurmas = qntddTurmas,
+      this.bairro = bairro,
+      this.cidade = cidade,
+      this.cep = cep,
+      this.telefone = telefone,
+      this.email = email,
+      this.nomeDoResponsavel = nomeDoResponsavel,
+      this.cargoDoResponsavel = cargoDoResponsavel
+
+  }
+
+  generateId() {
+    return Math.floor(Math.random() * 1000)
+  }
+}
